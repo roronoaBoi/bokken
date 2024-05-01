d4d3d0cf4fe4bae4cc4177a6a63f891ecdb6735c
roronoaBoi <robylee27@gmail.com>
2024-05-01 13:46:50 -0500

    added lockdown script

--- Scripts/express/lockInspection.js
+++ Scripts/express/lockInspection.js
@@ -2,39 +2,41 @@ const express = require('express')
 const app = express()
 const port = process.argv[2]
 const endPoint = process.argv[3]
-
+const token = process.argv[4]
 
 app.use(express.json());
 
 app.post('/' + endPoint, (req, res) => {
 
     async function process(data){
+        //instead of actionURL stuff, use the endpoint that removes inspection access from
+        //the completer... this shouldn't override permissions in SC, so it wont lock out
+        //an admin I think... reassign to an admin and make sure template rules harmonize
         const auditId = data.data.audit.audit_id
-        const user = data.event.triggered_by.name
-        const createActionUrl = 'https://api.safetyculture.io/tasks/v1/actions'
-        const createActionOptions = {
-            method: 'POST',
+        const userId = data.data.audit.audit_data.authorship.author_id
+        const removeAccessUrl = `https://api.safetyculture.io/inspections/v1/inspections/${auditId}/user/${userId}/access?new_owner_id=1d1afa78-7616-4734-ab5a-a5d6624f20f5`
+        const removeAccessOptions = {
+            method: 'DELETE',
             headers: {
                 accept: 'application/json',
                 'sc-integration-id': 'sc-readme',
                 'content-type': 'application/json',
                 authorization: 'Bearer ' + token
-            },
-            body: JSON.stringify({title: `${user} Inspection Follow Up`})
+            }
         };
-        console.log(`${auditId} completed by ${user}!`)
-        await fetch(createActionUrl,createActionOptions)
+        console.log(`${auditId} completed by ${userId}... removing access...`)
+        await fetch(removeAccessUrl,removeAccessOptions)
         .then((response) => response.json())
         .then(data => {
-            const actionId = data.action_id
-            console.log(`action ${actionId} created from inspection!`)
+            const inspectionId = data.inspection_id
+            console.log(`${inspectionId} locked!`)
         })
 
     }
 
     process(req.body)
 
-    res.status(200).send('Webhook received successfully!');
+    res.status(200).send('Webhook received successfully! Inspection locked!');
 });
 
 app.listen(port, () => {
