c25025803196250df09de55b2a406d516b5f940f
ro <robylee27@gmail.com>
2024-07-26 22:31:45 -0500

    working ex

---
+++ node_scripts/graphqlex/index.js
@@ -0,0 +1,52 @@
+
+const query = `
+query ($id: Int) {
+  Media(id: $id, type: ANIME) {
+    id
+    title {
+      romaji
+      english
+      native
+    }
+  }
+}
+`;
+
+
+const variables = {
+    id: 15127
+};
+
+
+const url = 'https://graphql.anilist.co',
+    options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+
+// Make the HTTP Api request
+fetch(url, options).then(handleResponse)
+                   .then(handleData)
+                   .catch(handleError);
+
+function handleResponse(response) {
+    return response.json().then(function (json) {
+        return response.ok ? json : Promise.reject(json);
+    });
+}
+
+function handleData(data) {
+    console.log(JSON.stringify(data, null, 2));
+}
+
+function handleError(error) {
+    alert('Error, check console');
+    console.error(error);
+}
