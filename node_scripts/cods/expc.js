c8e6e9b018f8c605b057bc608dd779d05b7b687c
roronoaBoi <robylee27@gmail.com>
2024-10-31 23:47:58 -0500

    linter

--- node_scripts/cods/expc.js
+++ node_scripts/cods/expc.js
@@ -10,6 +10,7 @@ const jsonMes = {
 app.get('/newPath', async function(req,res) {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
     const jsonResponse = await response.json()
+    console.log(jsonMes)
     res.send(jsonResponse)
 })
 app.listen(3000, () => console.log('app listening on localhost 3000...'))
